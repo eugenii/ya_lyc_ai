92a12e13c52849d12adad093f763f150a38872ba
eugenii <eugenii.cher@gmail.com>
2026-03-28 17:09:24 +0300

    g

--- 03_linear_regression/lesson_04/03_predict_model.py
+++ 03_linear_regression/lesson_04/03_predict_model.py
@@ -7,9 +7,22 @@ weights = np.array([float(i) for i in input().split()])
 
 real = np.array([float(i) for i in input().split()])
 
+
 def predict(x):
     return weights[0] + np.dot(weights[1:], x)
 
+
+def mse(y, y_pred):
+    return np.mean((y - y_pred) ** 2)
+
+
+predicts = []
+errors = []
 for _ in range(count):
     x = np.array([float(i) for i in input().split()])
-    print(predict(x))
+    predicts.append(predict(x))
+
+
+result = np.linalg.norm(real - predicts)
+
+print(result.round(2))
