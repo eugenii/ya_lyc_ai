10ca3251af384a438670832d964fd1ae0607381c
eugenii <eugenii.cher@gmail.com>
2026-05-04 19:42:44 +0300

    less_03 NLP

---
+++ 06_NLP/less_03/02.py
@@ -0,0 +1,14 @@
+import gensim.downloader as api
+
+# Загружаем Word2Vec, обученный на Google News (1.6 GB, может занять время)
+model = api.load("word2vec-google-news-300")
+
+print("Модель загружена!")
+# print(f"Размер словаря: {len(model)} слов")
+# print(f"Размерность вектора: {model.vector_size}")
+# print(f"Самое похожее на 'cat': {model.most_similar('cat')}")
+
+# similarity = model.similarity("cat", "dog")
+for  word in ('call', 'battery', 'mobile', 'screen', 'voice'):
+    similarity = model.similarity(word, "phone")
+    print(f"Схожесть '{word}' и 'phone': {similarity}")
